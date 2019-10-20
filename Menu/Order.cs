51089ce3be1e34ac6df222a03d8c9a71d02d1252
Ethan Zajac <ezajacthan@gmail.com>
2019-10-19 19:42:41 -0500

    Order implemented, but not debugged

---
+++ Menu/Order.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu
+{
+    public class Order
+    {
+        public ObservableCollection<IOrderItem> Items { get; set; }
+
+        public double SubtotalCost
+        {
+            get
+            {
+                double subtotal = 0.0;
+                foreach (IOrderItem item in this)
+                {
+                    subtotal += item.Price;
+                }
+                if (subtotal < 0) return 0;
+                else return subtotal;
+            }
+        }
+
+        public double SalesTaxRate { get; protected set; }
+
+        public double SalestTaxCost
+        {
+            get
+            {
+                return SalesTaxRate * SubtotalCost;
+            }
+        }
+
+        public double TotalCost
+        {
+            get { return this.SubtotalCost + SalestTaxCost; }
+        }
+    }
+}
